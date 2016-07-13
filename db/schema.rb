69eee8ec52f7de1244d576fbd1b4a526ab1c9e02
David Sabin <ddansab@gmail.com>
2016-07-13 17:19:52 -0400

    models and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713201330) do
+ActiveRecord::Schema.define(version: 20160713211809) do
+
+  create_table "boats", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "capacit"
+    t.string   "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "container"
+    t.string   "cargo"
+    t.string   "origin"
+    t.decimal  "cost"
+    t.string   "destination"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
