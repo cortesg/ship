27fb474cf11170fd45aa97c08e3411efc7b8e810
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-07-14 00:47:32 -0400

    extraneous info deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def signin
-    @user = User.find_by_email(params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        redirect_to :back
-      end
-  end
-
   def edit
   end
 
