0cc6becb07dbe315cc31425a0681b5a96e4d8fd0
Kisuk Yu <kisuk.ryu@gmail.com>
2016-07-18 16:48:48 -0400

    added in paperclip function

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714050709) do
+ActiveRecord::Schema.define(version: 20160718203650) do
 
   create_table "boats", force: :cascade do |t|
     t.string   "name"
     t.integer  "capacity"
     t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -36,8 +40,12 @@ ActiveRecord::Schema.define(version: 20160714050709) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
